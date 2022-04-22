0c693f697aee1974acbaa5f0cce1f7a5cc27cc8e
thebuidler <thebuidler@pm.me>
2022-04-22 09:46:23 -0500

    Added handler for TpUpdated event

--- src/handlers/GNSTradingCallbacksV6/index.ts
+++ src/handlers/GNSTradingCallbacksV6/index.ts
@@ -2,3 +2,5 @@ export { handleMarketExecuted } from "./MarketExecuted";
 export { handleMarketOpenCanceled } from "./MarketOpenCanceled";
 export { handleMarketCloseCanceled } from "./MarketCloseCanceled";
 export { handleLimitExecuted } from "./LimitExecuted";
+export { handleSlCanceled } from "./SlCanceled";
+export { handleSlUpdated } from "./SlUpdated";
--- src/handlers/GNSTradingV6/TpUpdated/index.ts
+++ src/handlers/GNSTradingV6/TpUpdated/index.ts
@@ -4,22 +4,19 @@ import {
   getTradesState,
   updateTradeAndTradeInfoToLatestFromTuple,
 } from "access/entity";
-import { SlUpdated } from "types/GNSTradingV6/GNSTradingV6";
+import { TpUpdated } from "types/GNSTradingV6/GNSTradingV6";
 
 /**
- * Event is emitted when a trade's stop loss is requested to be updated, and due to guaranteed SL rules on asset,
- * it's immediately granted.
- *
- * This event is not to be confused with GNSTradingCallbackV6.SlUpdated
+ * Event is emitted when a trade's take profit is updated.
  *
  * Basic flow:
  * a. update Trade obj
  * b. update TradeInfo obj
  *
- * @param event SlUpdated
+ * @param event TpUpdated
  */
-export function handleSlUpdated(event: SlUpdated): void {
-  const { trader, pairIndex, index, newSl } = event.params;
+export function handleTpUpdated(event: TpUpdated): void {
+  const { trader, pairIndex, index, newTp } = event.params;
 
   const tuple = { trader, pairIndex, index };
 
@@ -30,7 +27,7 @@ export function handleSlUpdated(event: SlUpdated): void {
     updateTradeAndTradeInfoToLatestFromTuple(state, storage, tuple, true);
   } catch (e) {
     log.error(
-      "[GNSTradingV6.handleSlUpdated] Error updating trade and tradeInfo for tuple {}",
+      "[handleTpUpdated] Error updating trade and tradeInfo for tuple {}",
       [JSON.stringify(tuple)]
     );
   }
